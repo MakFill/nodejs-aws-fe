5dc3f9b37a51997dfb6e30dc7317c84ed9979cf1
MakFil <makfil1177@gmail.com>
2021-08-04 20:26:08 +0300

    refactor: according chosen theme

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -1,17 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
-import {Product} from "models/Product";
-import {formatAsPrice} from "utils/utils";
-import AddProductToCart from "components/AddProductToCart/AddProductToCart";
+import { makeStyles } from '@material-ui/core/styles';
+import { Product } from 'models/Product';
+import { formatAsPrice } from 'utils/utils';
+import AddProductToCart from 'components/AddProductToCart/AddProductToCart';
 // import axios from 'axios';
 // import API_PATHS from "constants/apiPaths";
-import productList from "./productList.json";
+// import productList from './productList.json';
+import productList from './productList.json';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -20,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '148.25%',
   },
   cardContent: {
     flexGrow: 1,
@@ -39,28 +40,23 @@ export default function Products() {
     // axios.get(`${API_PATHS.bff}/product/available/`)
     //   .then(res => setProducts(res.data));
     setProducts(productList);
-  }, [])
+  }, []);
 
   return (
     <Grid container spacing={4}>
-      {products.map((product: Product, index: number) => (
+      {products.map((product: Product) => (
         <Grid item key={product.id} xs={12} sm={6} md={4}>
           <Card className={classes.card}>
-            <CardMedia
-              className={classes.cardMedia}
-              image={`https://source.unsplash.com/random?sig=${index}`}
-              title="Image title"
-            />
+            <CardMedia className={classes.cardMedia} image={product.image} title="Image title" />
             <CardContent className={classes.cardContent}>
               <Typography gutterBottom variant="h5" component="h2">
                 {product.title}
               </Typography>
-              <Typography>
-                {formatAsPrice(product.price)}
-              </Typography>
+              <Typography gutterBottom>{product.description}</Typography>
+              <Typography variant="h6">{formatAsPrice(product.price)}</Typography>
             </CardContent>
             <CardActions>
-              <AddProductToCart product={product}/>
+              <AddProductToCart product={product} />
             </CardActions>
           </Card>
         </Grid>
