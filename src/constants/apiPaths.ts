ad45c8936f9caf3d5ea50657c09c2839448f2187
MakFil <makfil1177@gmail.com>
2021-09-06 18:56:26 +0300

    refactor: change axios request path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://7g41nkxp27.execute-api.eu-west-1.amazonaws.com/dev/products',
+  bff: 'https://wkvzg727s4.execute-api.eu-west-1.amazonaws.com/prod/products',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
