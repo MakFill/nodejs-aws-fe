9e12d51bce3400834f07bbeca51a010d96159304
MakFil <makfil1177@gmail.com>
2021-09-07 16:38:46 +0300

    feat: implement task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://gwbdf8rcf3.execute-api.eu-west-1.amazonaws.com/prod',
   bff: 'https://wkvzg727s4.execute-api.eu-west-1.amazonaws.com/prod/products',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
