08f186065c526f304c07e9cba1c3fecd2781b7fe
MakFil <makfil1177@gmail.com>
2022-07-02 16:47:11 +0300

    [REFACTOR] Add imort api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://c5zgb151q2.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://k8dr3mlnwa.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
