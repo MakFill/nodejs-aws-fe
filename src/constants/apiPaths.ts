ebd443ad503100238a3c10c08638a7bb5a1d036d
MakFil <makfil1177@gmail.com>
2022-07-30 21:46:17 +0300

    [REFACTOR] Update cart endpoint with proxy link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://tg1u57frh7.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://k8dr3mlnwa.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://makfill-cart-api-prod.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://cxr59lrjga.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
