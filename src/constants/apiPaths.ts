370c0f8f76200af4a4cb520a934ca972affb825d
MakFil <makfil1177@gmail.com>
2022-07-30 21:14:48 +0300

    [REFACTOR] Update cart endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://tg1u57frh7.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://k8dr3mlnwa.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://makfill-cart-api-prod.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
